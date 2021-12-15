f5ceca7615498232b5af410bf5754ecc38736fee
Justin W Soto <soto@pdx.edu>
2021-12-14 17:13:53 -0800

    added entry and entry list components

--- src/components/Entry/Entry.jsx
+++ src/components/Entry/Entry.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 export const Entry = () => {
     return (
         <div>
-            
+            <h3>Your name</h3>
+            <section>Your message</section>
         </div>
     )
 }
--- src/components/EntryList/EntryList.jsx
+++ src/components/EntryList/EntryList.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { Entry } from '../Entry/Entry'
 
 export const EntryList = () => {
     return (
         <div>
-            
+            <ul>
+                <li><Entry/></li>
+            </ul>
         </div>
     )
 }
