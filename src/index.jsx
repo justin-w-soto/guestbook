dd5af189d963f0c8e14c9ea92b4d31fda06c0d01
Justin W Soto <soto@pdx.edu>
2021-12-14 19:42:43 -0800

    wrapped app component in userprovider component

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,13 @@
+import React from 'react';
 import { render } from 'react-dom';
 import App from './App';
+import { UserProvider } from './context/UserContext';
 
 render(
-<App />, 
-document.getElementById('root'));
+
+<UserProvider>
+<App />
+</UserProvider>,
+
+document.getElementById('root')
+);
