dae786abf54b3fdf72057096b2e434065238a074
Justin W Soto <soto@pdx.edu>
2022-01-03 17:30:03 -0800

    button and redirect working with private route

--- src/components/PrivateRoute/PrivateRoute.jsx
+++ src/components/PrivateRoute/PrivateRoute.jsx
@@ -1,9 +1,16 @@
 import { Route } from 'react-router-dom'
+import { Redirect } from 'react-router-dom'
+import { useMyHook } from '../../context/UserContext'
 
 export const PrivateRoute = ({ children, ...rest }) => {
+    const { user } = useMyHook();
     return (
-        <Route {...rest}>
-           <h1>This is private time shhhhh!</h1> {children}
-        </Route>
+
+        <>
+        <h1>This is private time shhhhh!</h1> 
+        <Route {...rest} render={( privateRouteProps ) => 
+            user ? children : <Redirect to={{pathname:'/login', 
+            state: { from: privateRouteProps.location } }} /> } />
+        </>
     )
 }
--- src/views/Auth/Auth.jsx
+++ src/views/Auth/Auth.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import { useLocation } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import { useMyHook } from '../../context/UserContext'
 
 export const Auth = () => {
-    return (
+    const { user, setUser } = useMyHook()
+    const location = useLocation()
+
+    const { from } = location.state || { from: { pathname: '/'} }
+
+     return (
         <div>
-            Login Page
+            <button onClick={() => setUser('bob')}>Log in as Bob</button>
+            Current User: {user}
+            <h3>Login Page</h3>
+            <p>Redirect Path: {from.pathname}</p>
+            <Link to="/">View Guestbook</Link>
         </div>
     )
 }
